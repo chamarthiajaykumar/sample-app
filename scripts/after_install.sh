9a980669726cd80a5ebff09ff3596edd43a2ae79
ajaykumarchamarthi03 <ajaykumarchamarthi@gmail.com>
2025-11-08 22:35:02 +0530

    changed after install script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "--- [AfterInstall] Copying new build to nginx web root ---"
 
-sudo cp -r /tmp/codedeploy-deploy/deploy/* /var/www/react/
+cp -r /tmp/codedeploy-deploy/* /var/www/react/
 
 sudo chown -R www-data:www-data /var/www/react
 sudo chmod -R 755 /var/www/react
